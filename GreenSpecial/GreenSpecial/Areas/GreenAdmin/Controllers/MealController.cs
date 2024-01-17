f5972e2222357881cadaade354cfb385e5493f2e
Asiman J. Gasimzada(MS) <asimanjg@code.local>
2024-01-17 18:35:03 +0400

    Identity Added

---
+++ GreenSpecial/GreenSpecial/Areas/GreenAdmin/Controllers/MealController.cs
@@ -0,0 +1,55 @@
+﻿using GreenSpecial.Areas.ViewModels;
+using GreenSpecial.DAL;
+using GreenSpecial.Models;
+using GreenSpecial.Utilities.Enums;
+using GreenSpecial.Utilities.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreenSpecial.Areas.GreenAdmin.Controllers
+{
+    [Area("GreenAdmin")]
+    public class MealController : Controller
+	{
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public MealController(AppDbContext context,IWebHostEnvironment env)
+		{
+            _context = context;
+            _env = env;
+        }
+		public async Task<IActionResult> Index()
+		{
+			List<Meal> meals = await _context.Meals.ToListAsync();
+			return View(meals);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Create(CreateMealVM mealVM)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View();
+			}
+			if (!mealVM.Photo.ValidateFileType(FileHelper.Image))
+			{
+				ModelState.AddModelError("Photo", "File type is incorrect");
+				return View();
+			}
+			if (!mealVM.Photo.ValidateFileSize(SizeHelper.mb))
+			{
+                ModelState.AddModelError("Photo", "File size is incorrect");
+                return View();
+            }
+			string filename=Guid.NewGuid().ToString()+mealVM.Photo.FileName;
+			string path = Path.Combine(_env.WebRootPath,"assets","uploads",filename);
+			FileStream file = new FileStream(path, FileMode.Create);
+			await mealVM.Photo.CopyToAsync(file);
+			return RedirectToAction("Index","Home");
+		}
+	}
+}
---
+++ GreenSpecial/GreenSpecial/Migrations/20240117113101_CreateDbandSettingandMealTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenSpecial.Migrations
+{
+    public partial class CreateDbandSettingandMealTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Key = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Meals");
+
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
